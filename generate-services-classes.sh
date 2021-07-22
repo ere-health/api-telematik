98e87ffe3def214b5eb135b0deb33ef365acb709
Manuel Blechschmidt <m.blechschmidt@mobizcorp.de>
2021-07-22 11:31:22 +0200

    Added generated VSD files (xjc -d src/main/java/ fa/vsds/Schema_VSD.xsd)

--- generate-services-classes.sh
+++ generate-services-classes.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wsimport -J-Djavax.xml.accessExternalDTD=all -d src/main/java conn/SignatureService.wsdl
+xjc -d src/main/java/ fa/vsds/Schema_VSD.xsd
+# wsimport -J-Djavax.xml.accessExternalDTD=all -d src/main/java conn/SignatureService.wsdl
